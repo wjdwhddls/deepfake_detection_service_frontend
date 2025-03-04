e2604ef8ce3cad04d5951a0b918cf26f29e746f8
theNorm <kisook2557@gmail.com>
2025-03-04 18:22:21 +0900

    feat : tailwind.config.js 파일 경로 설정 예시

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+/** @type {import('tailwindcss').Config} */  
+module.exports = {  
+  content: [  
+    "./src/**/*.{js,jsx,ts,tsx}", // src 폴더 내의 모든 JS, JSX, TS, TSX 파일  
+    "./public/index.html" // public 폴더의 index.html 파일  
+  ],  
+  theme: {  
+    extend: {},  
+  },  
+  plugins: [],  
+};
